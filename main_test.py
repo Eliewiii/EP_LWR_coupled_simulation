c5971baa3c9c3567e0c5a2a4a62c222a3796cf25
Eliewiii <elie.medioniwiii@gmail.com>
2024-11-17 16:01:17 +0200

    basic structure of a main test file to test the coupled simulation.

---
+++ main_test.py
@@ -0,0 +1,21 @@
+"""
+Main file to test the functionalities the coupled simulations.
+To be converted in to a real test file, but pytest might lead to problems with the multiprocessing/shared memory etc.
+"""
+
+def main ():
+    """
+
+    :return:
+    """
+    # Init all the files and paths
+
+    # generate the ep simulation instance objects
+
+    # init the Manager and the shared memory
+
+    # Run the simulation in parallel with concurrent.futures.ProcessPoolExecutor
+
+
+if __name__ == "__main__":
+    main()
