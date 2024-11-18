6c91c019c6eced89986231f0aa5c47eef60b8d09
elinux <elie-medioni@campus.technion.ac.il>
2024-11-18 18:21:47 +0200

    Class to manage the simulation : just starting Addons to consider shared memory

---
+++ src/lwrepcoulping/ep_coupled_simulation_manager.py
@@ -0,0 +1,80 @@
+"""
+Class to manage the couped long-wave radiation (LWR) simulation with EnergyPlus among multiple buildings.
+"""
+import numpy as np
+import os
+from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import shared_memory, Manager
+
+from .ep_simulation_instance_with_shared_memory import EpSimulationInstance
+
+
+class EpLwrSimulationManager:
+    def __init__(self, simulation, coupling):
+        # Simulation
+        self._building_id_list = []
+        self._ep_simulation_instance_list = []
+        self._path_idf_list = []  # Ordered according to building_id_list
+
+
+        # Input variables
+        self._path_output_dir = None
+        self._path_epw = None
+
+        #
+
+
+    #-----------------------------------------------------#
+    #--------------------- Properties --------------------#
+    #-----------------------------------------------------#
+
+    @property
+    def num_building(self):
+        return len(self._building_id_list)
+
+
+    def run_lwr_coupled_simulation(self):
+        """
+
+        :return:
+        """
+
+        #todo : identify the size of array_shape according to the number of outdoor surfaces of the buildings
+        # need to be in the proper format
+        array_shape= None
+
+        # Run the simulation under a Manager context to share memory, locks, and barriers
+        with Manager() as manager:
+
+            # Initialize a lock to limit writing access to shared memory
+            shared_memory_lock = manager.Lock()
+            # Initialize a barrier to synchronize processes, when called with .wait() all processes will wait until all
+            # processes have reached the barrier
+            synch_point_barrier = manager.Barrier(self.num_building)
+
+            # Create shared memory for float64 data (enough for all processes' lists)
+            shm = shared_memory.SharedMemory(create=True, size=array_shape[0] * np.float64().itemsize)
+
+            # Run the EnergyPlus simulations in parallel for all buildings, monitored by the EnergyPlus API
+            try:
+                # Start tasks
+                with ProcessPoolExecutor() as executor:
+                    futures = [
+                        executor.submit(
+                            process_task,
+                            shm.name,
+                            array_shape,
+                            lock,
+                            barrier,
+                            i
+                        )
+                        for i in range(num_processes)
+                    ]
+                    # Wait for all processes to complete
+                    for future in futures:
+                        future.result()
+
+            finally:
+                # Cleanup
+                shm.close()
+                shm.unlink()
---
+++ src/lwrepcoulping/ep_simulation_instance_with_shared_memory.py
@@ -0,0 +1,203 @@
+"""
+Class to represent an instance of a simulation of the EP model, generating the idf additional strings,
+managing the handlers etc.
+"""
+
+import os
+from multiprocessing import shared_memory
+from typing import List
+
+from src.pyenergyplus.api import EnergyPlusAPI
+
+
+class EpSimulationInstance:
+    """
+
+    """
+
+    def __init__(self, path_idf: str, path_epw: str, path_output_dir: str, path_energyplus_dir: str,
+                 simulation_index: int):
+        """
+
+        """
+        # initialize the EnergyPlus API and simulation state
+        self.api = EnergyPlusAPI(running_as_python_plugin=True, path_to_ep_folder=path_energyplus_dir)
+        self.state = self.api.state_manager.new_state()
+        #
+        self.path_idf = path_idf
+        self.path_epw = path_epw
+        self.path_output_dir = path_output_dir
+        self.schedule_actuator_handle_list = []
+        self.surface_temperature_handle_list = []
+
+        # Geometry
+        self.outdoor_surface_name_list = []
+        self.outdoor_surface_surrounding_surface_vf_dict = {}
+        self.outdoor_surface_sky_vf_dict = {}
+        self.outdoor_surface_ground_vf_dict = {}
+        # todo: potentially need to add the emissivity or other related parameters
+
+        # Surrounding surface temperature schedule
+        self.schedule_name_dict = {}
+
+        # Handlers
+        self.surface_temp_handler_dict = {}
+        self.surrounding_surface_temperature_schedule_temperature_handler_dict = {}
+
+        # LWR data
+        self.f_epsilon_matrix = None
+        self.f_matrix = None
+
+        # Synchronization attributes
+        self.simulation_index = simulation_index  # Index of the simulation, to synchronize the simulation and
+        # set the order of surface temperature reading to match the LWR computation matrix
+        self.shared_temperature_array = None
+
+    def generate_idf_with_additional_strings(self):
+        """
+
+        :param additional_strings:
+        :return:
+        """
+
+    def generate_additional_strings(self):
+        """
+
+        :return:
+        """
+
+    @staticmethod
+    def read_other_building_surface_temperature():
+        """
+
+        :return:
+        """
+
+    def request_variables_before_running_simulation(self):
+        """
+
+        :return:
+
+        A PRIORI DONE
+        """
+        for surface_name in self.outdoor_surface_name_list:
+            self.api.exchange.get_variable_handle(self.state, "SURFACE OUTSIDE FACE TEMPERATURE",
+                                                  surface_name)
+            self.api.exchange.get_variable_handle(self.state, "Schedule Value",
+                                                  self.schedule_name_dict[surface_name])
+
+    def initialize_actuator_handler_callback_function(self):
+        """
+
+        :return:
+        """
+        for surface_name in self.outdoor_surface_name_list:
+            schedule_actuator_handle = self.api.exchange.get_actuator_handle(self.state, "Schedule:Constant",
+                                                                             "Schedule Value",
+                                                                             self.schedule_name_dict[surface_name])
+            if schedule_actuator_handle == -1:
+                raise ValueError(f"Failed to create actuator for schedule {self.schedule_name_dict[surface_name]}")
+            else:
+                self.surrounding_surface_temperature_schedule_temperature_handler_dict[
+                    surface_name] = schedule_actuator_handle
+
+    def init_schedule_and_surface_temperature_handlers_call_back_function(self):
+        """
+
+        :return:
+        """
+
+    def get_surface_temperature_of_all_outdoor_surfaces(self) -> List[float]:
+        """
+        Reads the surface temperature of all the outdoor surfaces and store them in a list.
+        :return: list,  List of surface temperatures
+        """
+        surface_temperatures_list = []
+        for surface_temperature_handle in self.surface_temperature_handle_list:
+            surface_temperatures_list.append(
+                self.api.exchange.get_variable_value(self.state, surface_temperature_handle))
+        return surface_temperatures_list
+
+    def write_surface_temperatures_in_file(self, current_time):
+        """
+
+        :return:
+        """
+        surface_temperatures_list = self.get_surface_temperature_of_all_outdoor_surfaces()
+        # write down the surface temperatures in a file with tmp extension to avoid reading it before it is fully written
+        with open(os.path.join(self.path_output_dir, f"{current_time}_{self.simulation_index}.tmp"), 'a') as file:
+            file.write(f"{current_time} {surface_temperatures_list}\n")
+        # Rename the file to remove the .tmp extension
+        os.rename(os.path.join(self.path_output_dir, f"{current_time}_{self.simulation_index}.tmp"),
+                  os.path.join(self.path_output_dir, f"{current_time}_{self.simulation_index}.txt"))
+
+    def coupled_simulation_callback_function(self, state):
+        """
+        Function to run at the end (or beginning) of each time step, to update the schedule values and surrounding surface temperatures.
+        :return:
+        """
+
+        # Get current simulation time (in hours)
+        current_time = self.api.exchange.current_sim_time(state)
+        # Get the surface temperatures of all the surfaces
+        surface_temperatures_list = self.get_surface_temperature_of_all_outdoor_surfaces()
+        # write down the surface temperatures the shared memory
+        with lock:
+            # todo: need to indicated properly the start and end index of the shared memory
+            # Here we are writing the list as a slice of the shared memory
+            shared_array[index:index + len(float_list)] = float_list
+            print(f"Process {index} wrote data at index {index}")
+
+        # wait for the other building to write down its surface temperatures
+
+        # read the other building surface temperatures
+
+        # update the surrounding surface temperature schedules with the proper "mean radiant temperature" values
+
+        # Delete the file with the surface temperatures
+        # os.remove(os.path.join(self.path_output_dir,f"{current_time}_{self.simulation_index}.txt"))
+
+    def run_ep_simulation(self,shared_memory_name,shared_memory_array_shape, shared_memory_lock,
+                                          synch_point_barrier):
+        """
+
+        :return:
+        """
+        # Point to the shared memory
+        shm = shared_memory.SharedMemory(name=shared_memory_name)
+        shared_array = np.ndarray(array_shape, dtype=np.float64, buffer=shm.buf)
+        self.shared_temperature_array = shared_array
+
+
+        # request the variables to access schedule and surface temperature values during the simulation
+
+        # Set the callback functions to run at the various moment of the simulation
+        self.api.runtime.callback_after_new_environment_warmup_complete(self.state,
+                                                                        self.initialize_actuator_handler_callback_function)
+        self.api.runtime.callback_after_new_environment_warmup_complete(self.state,
+                                                                        self.init_schedule_and_surface_temperature_handlers_call_back_function)
+        self.api.runtime.callback_after_predictor_before_hvac_managers(self.state,
+                                                                       self.coupled_simulation_callback_function)  # todo: might be change to the end of the timestep
+
+        # Run the EnergyPlus simulation
+        self.api.runtime.run_energyplus(self.state,
+                                        ['-r',  # Run annual simulation
+                                         '-w', self.path_epw,  # Weather file
+                                         '-d', self.output_dir,  # Output directory
+                                         self.idf_file]  # Input IDF file
+                                        )
+
+
+def run_simulation_wrapper(simulation_instance: EpSimulationInstance, shared_memory_name: str,shared_memory_array_shape, shared_memory_lock,
+                           synch_point_barrier):
+    """
+
+    :param simulation_instance:
+    :param shared_memory_name:
+    :param shared_memory_lock:
+    :param synch_point_barrier:
+    :return:
+    """
+    simulation_instance.run_ep_simulation(shared_memory_name=shared_memory_name,shared_memory_array_shape=shared_memory_array_shape, shared_memory_lock=shared_memory_lock,
+                                          synch_point_barrier=synch_point_barrier)
+    return None
